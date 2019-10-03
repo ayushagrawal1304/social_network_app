8a1d6745bda0d60f59347c1d32f344f6979296c0
Ayush <ayagrawal@grepruby.io>
2019-10-03 20:21:25 +0530

    Added Accept and reject invitations

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -15,6 +15,20 @@ class Users::InvitationsController < ApplicationController
     @approvals=UserApproval.where(user_id: current_user.id).where(approved: false)
   end
 
+  def accept
+    if UserApproval.find_by(id: params[:id]).update(approved: true)
+      redirect_to users_invitations_path
+    else
+      render 'index'
+    end
+  end
+
+  def destroy
+    @approval = UserApproval.find(params[:id])
+    @approval.destroy
+    redirect_to users_invitations_path
+  end
+
   def approval_params
     params.permit(:user_id, :community_id)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,18 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :users do
-    resources :dashboards
-    resources :communities
-    resources :invitations
+    resources :dashboards   
+
+    resources :communities do
+      member do
+        get "invite_user"
+      end
+    end
+
+    resources :invitations do
+      member do
+        put "accept"
+      end
+    end
   end
 end

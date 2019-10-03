32ec4f2af386063d52e0528d4ca6a654402398ec
Ayush <ayagrawal@grepruby.io>
2019-10-03 18:26:16 +0530

    Added user profile page and its edits

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,10 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
   users_dashboards_index_path #your path
   end
-  
+
   protected
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up){ |u| u.permit(:name, :email, :password, :first_name, :password_confirmation, :last_name, :mobile_no)}
+      devise_parameter_sanitizer.permit(:account_update){ |u| u.permit(:name, :email, :password, :current_password, :first_name, :password_confirmation, :last_name, :mobile_no, :image)}
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,8 @@ Rails.application.routes.draw do
   
   devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :users do
+    resources :dashboards, only: :index
+  end
   
 end

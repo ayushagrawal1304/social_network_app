b76a33a70af77a5c9d01589d4c847d64a10a0c9b
Ayush <ayagrawal@grepruby.io>
2019-10-22 16:52:59 +0530

    Modified rspec for user_approval model

--- spec/models/user_approval_spec.rb
+++ spec/models/user_approval_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe UserApproval, :type => :model do
-  
   it { is_expected.to belong_to :community }
   it { is_expected.to belong_to :user }
-  
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,9 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'devise'
 ENV['RAILS_ENV'] ||= 'test'
+
+include Warden::Test::Helpers
 
 require File.expand_path('../config/environment', __dir__)
 
@@ -40,6 +43,8 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+
+  config.include Devise::Test::ControllerHelpers, type: :controller
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
